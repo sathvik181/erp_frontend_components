9a0378af469ecaeac8484587bd5558f4c5d50552
sathvik181 <sathvik@grahith.com>
2024-06-08 10:10:32 +0530

    dashboard

--- src/App.js
+++ src/App.js
@@ -2,16 +2,17 @@
 
 // import { Route, Routes } from 'react-router-dom';
 import './App.css';
+// import { CreateFeeStructure } from './modules/FeeManagementModule/FeeStructureModule/components/CreateFeeStructure'
+import { FeeDashBoard } from './modules/FeeManagementModule/FeeDashBoard';
 // import { AddFeeDetails } from './components/AddFeeStructure/AddFeeDetails';
 // import { DetailsCard } from './components/DetailsCard/DetailsCard';
-import { CreateFeeStructure } from './modules/FeeManagementModule/FeeStructureModule/components/CreateFeeStructure'
 // import { CreateFeeStructure } from './modules/FeeManagementModule/FeeStructureModule/components/AddFeeStructure/CreateFeeStructure';
 // import { CreateFeeStructure } from './components/AddFeeStructure/CreateFeeStructure';
 function App() {
   return (
     <div className="App">
 
-    <CreateFeeStructure />
+    <FeeDashBoard />
      
     </div>
   );
